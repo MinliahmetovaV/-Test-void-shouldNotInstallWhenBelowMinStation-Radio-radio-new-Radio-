71e3cdaa21a5a5222537e8f59c8fe3f5d8995c6f
Minliahmetova Violetta <vettaify@gmail.com>
2022-10-22 15:12:20 +0600

    initial commit

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -3,16 +3,119 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RadioTest {
+    Radio radio = new Radio();
     @Test
     void shouldNotInstallWhenBelowMinStation() {
-        Radio radio = new Radio();
         radio.setCurrentStation(-1);
         assertEquals(0,radio.getCurrentStation());
     }
     @Test
     void shouldInstallWhenMaxStation() {
-        Radio radio = new Radio();
         radio.setCurrentStation(9);
         assertEquals(9, radio.getCurrentStation());
     }
+    @Test
+    void shouldNotInstallWhenAboveMaxStation() {
+        radio.setCurrentStation(10);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void nextStationShouldThrowExceptionWhenMaxStation() {
+        radio.setCurrentStation(9);
+        radio.next();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void nextStationShouldTChangeWhenValidStation() {
+        radio.setCurrentStation(8);
+        radio.next();
+        assertEquals(9, radio.getCurrentStation());
+    }
+
+
+    @Test
+    void prevStationShouldThrowExceptionWhenMinStation() {
+        radio.setCurrentStation(0);
+        radio.prev();
+        assertEquals(9, radio.getCurrentStation());
+    }
+
+    @Test
+    void prevStationShouldChangeWhenMaxStation() {
+        radio.setCurrentStation(9);
+        radio.prev();
+        assertEquals(8, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldInstallStationBySpecifyingNumber() {
+        Radio radio = new Radio(9);
+        radio.setCurrentStation(3);
+        assertEquals(3, radio.getCurrentStation());
+    }
+
+
+    @Test
+    void shouldNotInstallWhenBelowMinVolume() {
+        radio.setCurrentVolume(-1);
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+
+    @Test
+    void shouldInstallWhenMinVolume() {
+        radio.setCurrentVolume(0);
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldInstallWhenMaxVolume() {
+        radio.setCurrentVolume(10);
+        assertEquals(10, radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldNotInstallWhenAboveMaxVolume() {
+        radio.setCurrentVolume(11);
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    void increaseVolumeShouldChangeWhenMinVolume() {
+        radio.setCurrentVolume(0);
+        radio.increaseVolume();
+        assertEquals(1, radio.getCurrentVolume());
+    }
+
+    @Test
+    void increaseVolumeShouldChangeWhenValidVolume() {
+        radio.setCurrentVolume(9);
+        radio.increaseVolume();
+        assertEquals(10, radio.getCurrentVolume());
+    }
+
+    @Test
+    void increaseVolumeShouldNotChangeWhenMaxVolume() {
+        radio.setCurrentVolume(10);
+        radio.increaseVolume();
+        assertEquals(10, radio.getCurrentVolume());
+    }
+
+    @Test
+    void decreaseVolumeShouldNotChangeWhenMinVolume() {
+        radio.setCurrentVolume(0);
+        radio.decreaseVolume();
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+
+    @Test
+    void decreaseVolumeShouldChangeWhenMaxVolume() {
+        radio.setCurrentVolume(10);
+        radio.decreaseVolume();
+        assertEquals(9, radio.getCurrentVolume());
+    }
+
 }
